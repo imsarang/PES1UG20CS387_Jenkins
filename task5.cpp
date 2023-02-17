e07af49476470c9c1b30427812bdb86f5ecec93a
imsarang <84568747+imsarang@users.noreply.github.com>
2023-02-17 21:49:50 +0530

    Update task5.cpp

--- task5.cpp
+++ task5.cpp
@@ -3,8 +3,6 @@ using namespace std;
 
 int main(void) {
   cout << "Hello, World\n";
-  cout << "Hello, Jenkins\n";
-  cout << "I have successfully built and run\n";
-  cout<<"Sarang Chilkund \t PES1UG20CS387 \n";
+  cout<<"Sarang Chilkund \t PES1UG20CS387 committed new file \n";
   return 0;
 }
