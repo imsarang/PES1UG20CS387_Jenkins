7ec546ea8ff974c30dbcb7e7d5218561b1361a45
imsarang <84568747+imsarang@users.noreply.github.com>
2023-02-17 21:28:08 +0530

    Update hello.cpp

--- main/hello.cpp
+++ main/hello.cpp
@@ -6,5 +6,6 @@ int main(void) {
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
   cout << "I have successfully built and run\n";
+  cout<<"Sarang Chilkund \t PES1UG20CS387 \n";
   return 0;
 }
